e1b348e47a2097fa145ed552b8bce07bf5fe72c7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-04-17 15:12:52 -0400

    initial code

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,5 +1,6 @@
 import { LoginInfo } from '.'
 
+// use these selectors to find elements on the Login page
 const selectors = {
   username: '[data-test="username"]',
   password: '[data-test="password"]',
@@ -14,51 +15,32 @@ describe('Login', () => {
   beforeEach(() => {
     // visit the login page
     // https://on.cypress.io/visit
-    cy.visit('/')
   })
 
   it('wrong password', () => {
     // type the valid username and some made up password
     // https://on.cypress.io/get
     // https://on.cypress.io/type
-    cy.get(selectors.username).type(user.username)
-    cy.get(selectors.password).type('incorrect-password')
+    //
     // click on the login button
     // https://on.cypress.io/click
-    cy.get(selectors.loginButton).click()
+    //
     // confirm the page shows errors and stays on login URL
     // https://on.cypress.io/contains
-    cy.contains(
-      selectors.error,
-      'Epic sadface: Username and password do not match any user in this service',
-    )
     // https://on.cypress.io/location
-    cy.location('pathname').should('equal', '/')
   })
 
   it('wrong username and password', () => {
     // make up both username and password
-    cy.get(selectors.username).type('user-not-found')
-    cy.get(selectors.password).type('incorrect-password')
     // click on the login button
     // https://on.cypress.io/click
-    cy.get(selectors.loginButton).click()
     // confirm the page shows errors and stays on login URL
-    cy.contains(
-      selectors.error,
-      'Epic sadface: Username and password do not match any user in this service',
-    )
-    cy.location('pathname').should('equal', '/')
   })
 
   it('successful logs in', () => {
     // type the valid username and password
-    cy.get(selectors.username).type(user.username)
-    cy.get(selectors.password).type(user.password)
     // click on the login button
     // https://on.cypress.io/click
-    cy.get(selectors.loginButton).click()
     // confirm the page redirects to /inventory
-    cy.location('pathname').should('equal', '/inventory')
   })
 })
