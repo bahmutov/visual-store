40dd8a097e4a61a9ace61e322968921301e7cf7a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-04-17 22:35:00 -0400

    a3

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -16,30 +16,30 @@ describe('Login', () => {
     cy.visit('/')
   })
 
-  it('wrong password', () => {
+  it('changes the error color from the test', () => {
     cy.get(selectors.username).type(user.username)
     cy.get(selectors.password).type('incorrect-password')
     cy.get(selectors.loginButton).click()
-    // confirm the page shows errors and stays on login URL
-    // https://on.cypress.io/contains
     cy.contains(
       selectors.error,
       'Epic sadface: Username and password do not match any user in this service',
     )
-    cy.location('pathname').should('equal', '/')
     // confirm the error message is red
     // (the background color is rgb(226, 35, 26))
     const redColor = 'rgb(226, 35, 26)'
     cy.get('.error-message-container')
       .then(($el) => window.getComputedStyle($el[0]).backgroundColor)
       .should('equal', redColor)
-    // confirm the username input has the error class
-    cy.get(selectors.username)
-      .should('have.class', 'error')
-      // and the red bottom border line
-      .then(($el) => window.getComputedStyle($el[0]).borderBottomColor)
-      .should('equal', redColor)
-    // tip: look up using computed style in the browser
-    // https://glebbahmutov.com/cypress-examples/recipes/computed-style.html
+    // from the test change the background color of the error element
+    // to green color and confirm the computed style is green
+    // Tip: all Cypress query commands yield a jQuery object
+    cy.get('.error-message-container').invoke(
+      'css',
+      'background-color',
+      'green',
+    )
+    cy.get('.error-message-container')
+      .then(($el) => window.getComputedStyle($el[0]).backgroundColor)
+      .should('equal', 'rgb(0, 128, 0)')
   })
 })
