b113e02bb81b0cc80c81141b87ca95d399f4d72c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-04-18 11:16:34 -0400

    a8

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,4 +1,6 @@
 import { LoginInfo } from '.'
+// https://github.com/bahmutov/cypress-map
+import 'cypress-map'
 
 // use these selectors to find elements on the Login page
 const selectors = {
@@ -16,24 +18,7 @@ describe('Login', () => {
     cy.visit('/')
   })
 
-  it('freezes all CSS animations', () => {
-    // insert a style tag into the body of the page
-    // that freezes all CSS animations
-    // by setting them to "none !important"
-    // See "How Cypress Freezes CSS Animations And You Can Too"
-    // https://glebbahmutov.com/blog/css-animations/
-    cy.get('body').invoke(
-      'append',
-      Cypress.$(`
-      <style id="__cypress-animation-disabler">
-        *, *:before, *:after {
-          transition-property: none !important;
-          animation: none !important;
-        }
-      </style>
-    `),
-    )
-
+  it('waits for the CSS property to stop changing', () => {
     cy.get(selectors.username).type(user.username)
     cy.get(selectors.password).type('incorrect-password')
     cy.get(selectors.loginButton).click()
@@ -42,23 +27,9 @@ describe('Login', () => {
       'Epic sadface: Username and password do not match any user in this service',
     )
     // get the error message container
-    // and assert that its background color is "rgb(226, 35, 26)"
-    // First, use the "window.getComputedStyle"
-    // Second, use cy.invoke('css', 'background-color')
-    // Which method worked?
-
-    // 1: using window.getComputedStyle works!
-    cy.log('**getComputedStyle**')
-    cy.get('.error-message-container')
-      .then(($el) => {
-        return window.getComputedStyle($el[0]).backgroundColor
-      })
-      .should('equal', 'rgb(226, 35, 26)')
-
-    // 2: using cy.invoke also works!
-    cy.log('**invoke css**')
-    cy.get('.error-message-container')
-      .invoke('css', 'background-color')
-      .should('equal', 'rgb(226, 35, 26)')
+    // and assert that its background color is stable
+    // that is, it's not changing anymore. You do not know the expected color.
+    // Tip: look up cy.stable command from the cypress-map plugin
+    cy.get('.error-message-container').stable('css', 'background-color', 100)
   })
 })
