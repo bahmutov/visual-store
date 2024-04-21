bcacc2087739127af26e765230004fc37d1e8aae
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-04-21 18:03:57 -0400

    b8

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -8,19 +8,7 @@ describe('Login', () => {
     // get the name of the rendered font
     // for an text element with actual text
     // (not just top level body)
-    cy.getCDPNodeId('.login_credentials').then((nodeId: number) => {
-      cy.CDP('CSS.getPlatformFontsForNode', {
-        nodeId,
-      })
-        // confirm the first rendered font is "Satisfy"
-        .its('fonts')
-        .should('not.be.empty')
-        .its(0)
-        .should('deep.include', { familyName: 'Satisfy' })
-        // if you want to print the font family name
-        // to the Command Log
-        .its('familyName')
-        .should('be.a', 'string')
-    })
+    //
+    // confirm the first rendered font is "Satisfy"
   })
 })
