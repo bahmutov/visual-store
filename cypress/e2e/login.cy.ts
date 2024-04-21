bae1225cb792abefceab08c242917193423f933e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-04-21 17:55:59 -0400

    b8

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,30 +1,26 @@
-import { LoginInfo } from '.'
-
-// use these selectors to find elements on the Login page
-const selectors = {
-  username: '[data-test="username"]',
-  password: '[data-test="password"]',
-  loginButton: '[data-test="login-button"]',
-  error: '[data-test="error"]',
-}
-
 describe('Login', () => {
-  // this is a valid user object with username and password
-  const user: LoginInfo = Cypress.env('users').standard
-
   beforeEach(() => {
     cy.visit('/')
   })
 
-  it('logs in', () => {
-    // how many screenshots does this test need?
-    // set up several screenshots and save them to the "cypress/gold" folder
-    cy.get('.error-message-container').should('be.empty')
-    cy.screenshot('login-page', { overwrite: true })
-    cy.get(selectors.username).type(user.username)
-    cy.get(selectors.password).type('wrong-password')
-    cy.get(selectors.loginButton).click()
-    cy.get('.error-message-container').should('not.be.empty')
-    cy.screenshot('login-error', { overwrite: true })
+  it('prints the rendered font', () => {
+    // using cypress-cdp plugin
+    // get the name of the rendered font
+    // for an text element with actual text
+    // (not just top level body)
+    cy.getCDPNodeId('.login_credentials').then((nodeId: number) => {
+      cy.CDP('CSS.getPlatformFontsForNode', {
+        nodeId,
+      })
+        // confirm the first rendered font is "Satisfy"
+        .its('fonts')
+        .should('not.be.empty')
+        .its(0)
+        .should('deep.include', { familyName: 'Satisfy' })
+        // if you want to print the font family name
+        // to the Command Log
+        .its('familyName')
+        .should('be.a', 'string')
+    })
   })
 })
