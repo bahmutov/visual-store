4d7dac19afa170d1b903acb1f68e1cbb9d4f49c5
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-04-22 13:06:55 -0400

    comments

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -4,6 +4,11 @@ describe('Login', () => {
   })
 
   it('prints the rendered font', () => {
+    // confirm the login form is visible
+    // run the custom command "cy.imageDiff"
+    // to compare the current page to the gold image
+    // Tip: find the custom command TS definition in cypress/support/index.d.ts
+    // and the implementation in cypress/support/commands.ts
     cy.get('.login_wrapper').should('be.visible')
     cy.imageDiff('login-page')
   })
