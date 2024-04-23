3763a18a2d6a1c63536ae895ac3d1cc9173f6e8f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-04-22 22:21:22 -0400

    c6

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -9,20 +9,8 @@
 // but then replace it with synthetic constant
 // and then take a snapshot
 it('buys an item', () => {
-  // solution 1: use synthetic time
-  cy.clock(new Date(2020, 1, 14, 12, 34, 56))
   cy.visit('/')
   cy.get('.login_wrapper').should('be.visible')
-  cy.imageDiff('login')
-})
-
-it('buys an item (replace content)', () => {
-  // solution 2: replace the dynamic text
-  cy.visit('/')
-  cy.get('.login_wrapper').should('be.visible')
-  // confirm the dynamic text has the right format
-  cy.contains('#last-login', /^\d\d:\d\d:\d\d [AP]M$/)
-    // and then replace the dynamic text with static timestamp
-    .invoke('text', '11:22:33 AM')
-  cy.imageDiff('login2')
+  // cannot take a screenshot since it is always different
+  // cy.imageDiff('login')
 })
