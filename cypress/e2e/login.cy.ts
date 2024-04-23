ce911e14b994abdfed0b8d535c69ff1bb68d0bd5
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-04-23 12:40:46 -0400

    c7

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -20,32 +20,26 @@ it('shows the stats before login (stub the network call)', () => {
   // stub the network call and give it an alias
   // https://on.cypress.io/intercept
   // https://on.cypress.io/as
-  cy.intercept(apiServerUrl, mockData).as('getStats')
   cy.visit('/')
   // confirm the network call was made
-  cy.wait('@getStats')
   // confirm the page shows the fetched data
   cy.get('#login_stats').should('be.visible')
   // visual assertion
-  cy.imageDiff('stats-mock')
+  // cy.imageDiff('stats-mock')
 })
 
 it('shows the stats before login (confirm the fetched data is shown)', () => {
   // do not stub the network call and give it an alias
   // https://on.cypress.io/intercept
   // https://on.cypress.io/as
-  cy.intercept(apiServerUrl).as('getStats')
   cy.visit('/')
   // confirm the page shows the fetched data
-  cy.wait('@getStats')
-    .its('response.body')
-    .then((data) => {
-      cy.get('#login_stats').should('be.visible')
-      // confirm the server response is shown
-      // and replace each field on the page with mock data
-      cy.contains('.total-items', data.totalItems).invoke('text', '777')
-      cy.contains('.last-item', data.lastSoldItem).invoke('text', 'Plasma TV')
-    })
+  // https://on.cypress.io/wait
+  // https://on.cypress.io/its
+
+  cy.get('#login_stats').should('be.visible')
+  // confirm the server response is shown
+  // and replace each field on the page with mock data
   // now the data is stable and we can take the screenshot
-  cy.imageDiff('stats-replaced')
+  // cy.imageDiff('stats-replaced')
 })
