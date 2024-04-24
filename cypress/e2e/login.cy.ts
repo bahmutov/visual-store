32e549d51fd9d5eeb0a95191fc691b5c6e9b6d55
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-04-24 11:47:10 -0400

    d5

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,10 +1,23 @@
-it('takes several login screenshots', () => {
+it('handles the difference in robot images', () => {
+  // intercept the outgoing request to any of the robot images
+  // that have form like '**/Login_Bot_graphic*.png'
+  // and replace it with the exact file name 'Login_Bot_graphic.png'
+  // so it always returns the first bot image
+  // https://on.cypress.io/intercept
+  cy.intercept('GET', '**/Login_Bot_graphic*.png', (req) => {
+    const sameUrl = req.url.replace(
+      /Login_Bot_graphic.*\.png/,
+      'Login_Bot_graphic.png',
+    )
+    console.log('replacing url', req.url, 'with', sameUrl)
+    req.url = sameUrl
+  })
+    // give the intercept an alias "botImage"
+    .as('botImage')
   cy.visit('/')
   cy.get('#login_button_container').should('be.visible')
-  cy.imageDiff('01-login-page', { mode: 'async' })
-  cy.get('#user-name').type('wrong username')
-  cy.get('#password').type('wrong password')
-  cy.get('#login-button').click()
-  cy.get('.error-message-container').should('be.visible')
-  cy.imageDiff('02-login-page-error', { mode: 'async' })
+  // confirm the bot image intercept was used
+  // https://on.cypress.io/wait
+  cy.wait('@botImage')
+  cy.imageDiff('01-login-page', { mode: 'sync' })
 })
