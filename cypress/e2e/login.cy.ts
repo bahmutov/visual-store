49ad0c3bdbc631dbc21edf2aaeab3993dc2d18ef
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-04-24 12:40:19 -0400

    d6

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -3,15 +3,10 @@ it('handles the difference in robot images', () => {
   // that have form like '**/Login_Bot_graphic*.png'
   // and replace it with the fixture image "robot-placeholder.png"
   // https://on.cypress.io/intercept
-  cy.intercept('GET', '**/Login_Bot_graphic*.png', {
-    fixture: 'robot-placeholder.png',
-  })
-    // give the intercept an alias "botImage"
-    .as('botImage')
+  // give the intercept an alias "botImage"
   cy.visit('/')
   cy.get('#login_button_container').should('be.visible')
   // confirm the bot image intercept was used
   // https://on.cypress.io/wait
-  cy.wait('@botImage')
   cy.imageDiff('01-login-page', { mode: 'sync' })
 })
