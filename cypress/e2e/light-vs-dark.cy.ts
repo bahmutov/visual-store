8922cbc5f4dd77f3f3575ba959d3b98f4955e3f5
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-04-23 23:02:08 -0400

    d2

--- cypress/e2e/light-vs-dark.cy.ts
+++ cypress/e2e/light-vs-dark.cy.ts
@@ -4,28 +4,12 @@ it('confirms the light and dark color themes', () => {
   // https://chromedevtools.github.io/devtools-protocol/tot/Emulation/
   // Tip: read the blog post "Emulate Media In Cypress Tests"
   // https://glebbahmutov.com/blog/cypress-emulate-media/
-  cy.CDP('Emulation.setEmulatedMedia', {
-    media: 'page',
-    features: [
-      {
-        name: 'prefers-color-scheme',
-        value: 'dark',
-      },
-    ],
-  })
+  //
   cy.visit('/')
   // prevent CSS regressions by taking an image diff
-  cy.imageDiff('login-dark')
+  // cy.imageDiff('login-dark')
   // switch back to the light color scheme
-  cy.CDP('Emulation.setEmulatedMedia', {
-    media: 'page',
-    features: [
-      {
-        name: 'prefers-color-scheme',
-        value: 'light',
-      },
-    ],
-  })
+  //
   // and take another image diff
-  cy.imageDiff('login-light')
+  // cy.imageDiff('login-light')
 })
