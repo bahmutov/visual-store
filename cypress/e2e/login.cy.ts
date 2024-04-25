6fc4c292a1a20d50dac0ebb2d72f50fd338f42d6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-04-25 14:39:28 -0400

    e4

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,37 +1,19 @@
 describe('Login page', () => {
-  it(
-    'looks good in desktop resolution',
-    // run this test in the desktop resolution 1024x768
-    // https://on.cypress.io/viewport
-    { viewportWidth: 1024, viewportHeight: 768 },
-    () => {
-      cy.visit('/')
-      // confirm the login page has loaded
-      // and take a screenshot using cy.imageDiff
-      // ignore the dynamic robot image
-      cy.get('.login_wrapper').should('be.visible')
-      cy.imageDiff('login-desktop', {
-        mode: 'sync',
-        ignoreElements: '.bot_column',
-      })
-    },
-  )
+  // https://on.cypress.io/viewport
+  // run this test in the desktop resolution 1024x768
+  it('looks good in desktop resolution', () => {
+    cy.visit('/')
+    // confirm the login page has loaded
+    // and take a screenshot using cy.imageDiff
+    // ignore the dynamic robot image
+  })
 
-  it(
-    'looks good in mobile resolution',
-    // run this test in the mobile resolution 375x667
-    // https://on.cypress.io/viewport
-    { viewportWidth: 375, viewportHeight: 667 },
-    () => {
-      cy.visit('/')
-      // confirm the login page has loaded
-      // and take a screenshot using cy.imageDiff
-      // ignore the dynamic robot image
-      cy.get('.login_wrapper').should('be.visible')
-      cy.imageDiff('login-mobile', {
-        mode: 'sync',
-        ignoreElements: '.bot_column',
-      })
-    },
-  )
+  // https://on.cypress.io/viewport
+  // run this test in the mobile resolution 375x667
+  it('looks good in mobile resolution', () => {
+    cy.visit('/')
+    // confirm the login page has loaded
+    // and take a screenshot using cy.imageDiff
+    // ignore the dynamic robot image
+  })
 })
