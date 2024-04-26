93322cda1eda29ada7fb50a08d12155f78306491
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-04-26 16:42:24 -0400

    add comments

--- src/components/Button.cy.jsx
+++ src/components/Button.cy.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Button from './Button'
 
-it.only('shows a button', () => {
+it('shows a button', () => {
   // mount the "Button" component and pass
   // the prop "label" with value "Test button"
   // Tip: use the cy.mount command, see
@@ -10,6 +10,8 @@ it.only('shows a button', () => {
   cy.mount(<Button label="Test button" />)
   // confirm the button with text "Test button"
   // is present on the page
+  // https://on.cypress.io/contains
+  // and make a visual assertion "button-default"
   cy.contains('button', 'Test button')
   cy.imageDiff('button-default')
 })
