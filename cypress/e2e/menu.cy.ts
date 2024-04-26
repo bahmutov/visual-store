030fda6ce1eed08c8f15a7694d96a09ec969bcf5
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-04-26 09:55:58 -0400

    hover

--- cypress/e2e/menu.cy.ts
+++ cypress/e2e/menu.cy.ts
@@ -1,3 +1,6 @@
+// https://github.com/dmtrKovalenko/cypress-real-events
+import 'cypress-real-events'
+
 it('captures the opened menu', () => {
   cy.visit('/')
   cy.get('[data-test="username"]').type('standard_user')
@@ -8,9 +11,17 @@ it('captures the opened menu', () => {
   // open the menu by clicking on the button
   // and confirm the ".bm-menu-wrap" is visible
   cy.contains('button', 'Open Menu').click()
-  cy.get('.bm-menu-wrap').should('be.visible')
-  cy.log('**menu opened 2**')
+  cy.get('.bm-menu-wrap')
+    .should('be.visible')
+    // we use a wait timeout because the menu has 0.5s transition
+    // and we need it to finish before we can hover
+    .stable('css', 'width')
+  cy.log('**menu opened**')
+
+  // hover over a menu item "About" which triggers the hover state
+  cy.contains('.menu-item', 'About').realHover()
+
   // capture and diff the current viewport only
   // without capturing the full footer element
-  cy.imageDiff('menu', { capture: 'clipToViewport' })
+  cy.imageDiff('menu-hover', { capture: 'clipToViewport' })
 })
