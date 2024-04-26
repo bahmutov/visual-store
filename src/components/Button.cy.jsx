4d3f626c796c234bfa2497413049dd7a9dc61f3a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-04-26 17:23:48 -0400

    f2

--- src/components/Button.cy.jsx
+++ src/components/Button.cy.jsx
@@ -1,69 +1,14 @@
 import React from 'react'
 import Button from './Button'
 
-it.only('shows a button', () => {
-  // mount the "Button" component and pass
-  // the prop "label" with value "Test button"
-  // Tip: use the cy.mount command, see
-  // https://on.cypress.io/mount
-  // and cypress/support/component.jsx
+it('shows a button', () => {
   cy.mount(<Button label="Test button" />)
-  // confirm the button with text "Test button"
-  // is present on the page
-  // https://on.cypress.io/contains
-  // and make a visual assertion "button-default"
   cy.contains('button', 'Test button')
   // before taking the screenshot, make the viewport just enough
   // to show the button by itself
   // Tip: get the getBoundingClientRect of the "html" element
   // and set the viewport to its width and height
   // https://on.cypress.io/viewport
-  cy.get('html').then(($el) => {
-    const rect = $el[0].getBoundingClientRect()
-    cy.viewport(Math.ceil(rect.width), Math.ceil(rect.height))
-  })
+  //
   cy.imageDiff('button-default')
-})
-
-it('calls the click prop', () => {
-  // mount the Button component
-  // with label "Test button" and "onClick" prop
-  // The "onClick" prop should be a Cypress Sinon stub function
-  // with an alias "click"
-  // https://on.cypress.io/stub
-  // https://on.cypress.io/as
-  cy.mount(<Button label="Test button" onClick={cy.stub().as('click')} />)
-  // query the page to find the button with text "Test button"
-  // and click on it
-  // https://on.cypress.io/contains
-  // https://on.cypress.io/click
-  cy.contains('button', 'Test button').click()
-  // get the "click" stub and confirm it was clicked once
-  // https://on.cypress.io/get
-  // https://glebbahmutov.com/cypress-examples/commands/spies-stubs-clocks.html
-  cy.get('@click').should('have.been.calledOnce')
-})
-
-it('does not click the disabled button', () => {
-  // mount the Button component
-  // passing the "disabled" prop and on click handler stub
-  // The "onClick" prop should be a Cypress Sinon stub function
-  // with an alias "click"
-  // https://on.cypress.io/stub
-  // https://on.cypress.io/as
-  cy.mount(<Button label="disabled" disabled onClick={cy.stub().as('click')} />)
-  // query the button by text and confirm it is disabled
-  // https://glebbahmutov.com/cypress-examples/commands/assertions.html
-  cy.contains('button', 'disabled')
-    .should('be.disabled')
-    // Change Button.css to not allow cursor
-    // when _any_ button is disabled
-    // and confirm the button in this test has the expected cursor CSS
-    .and('have.css', 'cursor', 'not-allowed')
-    // click the button
-    // Tip: you need to force Cypress to click the disabled element
-    .click({ force: true })
-  // get the click prop stub by its alias
-  // and confirm it was never called
-  cy.get('@click').should('not.be.called')
 })
