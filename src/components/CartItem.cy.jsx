14f144a35fe116d77fb3a4bf5e93674d9b46f927
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-04-27 11:25:49 -0400

    f4

--- src/components/CartItem.cy.jsx
+++ src/components/CartItem.cy.jsx
@@ -15,23 +15,18 @@ describe('CartItem', () => {
   it('removes the item from the cart', () => {
     // mount the cart item (with the router), passing the item as a prop
     // and pass the "showButton: true" prop
-    cy.mountWithRouter(<CartItem item={item} showButton={true} />)
+    cy.mountWithRouter(<CartItem item={item} />)
     // confirm using a functional assertion that "Remove" button is visible
-    cy.contains('button', 'Remove').should('be.visible')
     // take a snapshot of the cart item
-    cy.imageDiff('CartItem-remove-button')
 
     // spy on the "ShoppingCart.removeItem" method
     // https://on.cypress.io/spy
     // and give the spy an alias "removeItem"
     // https://on.cypress.io/as
     // https://glebbahmutov.com/cypress-examples/commands/spies-stubs-clocks.html
-    cy.spy(ShoppingCart, 'removeItem').as('removeItem')
     // click the "Remove" button
-    cy.contains('button', 'Remove').click()
+    //
     // confirm the entire cart item is gone
-    cy.get('.cart_item').should('not.exist')
     // confirm the spy was called with the correct arguments
-    cy.get('@removeItem').should('have.been.calledWithExactly', item.id)
   })
 })
